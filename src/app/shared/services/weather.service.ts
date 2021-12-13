25fa53d3ebd5610a6c9b0165737039ca5a8b43e6
Luca861 <luca.carapellese@gmail.com>
2021-12-13 12:13:26 +0100

    feat add test

--- src/app/shared/services/weather.service.ts
+++ src/app/shared/services/weather.service.ts
@@ -14,7 +14,7 @@ export class WeatherService {
 
 
 getCityWeatherByName(selectedCity:string):Observable<any>{
-  const params = new HttpParams().set('q',selectedCity).set('appid','51f71d7c995963ad8a76fecb16ed393b')
+  const params = new HttpParams().set('q',selectedCity).set('appid','51f71d7c995963ad8a76fecb16ed393b').set('units','metric')
   return this.http.get<any>('http://api.openweathermap.org/data/2.5/weather', {params})
   .pipe(
     map( response => [response.name,response.main.temp]))
